453e4ea4c648db8f27073222a4b995fd4bbed859
김병민 <gimbyeongmin@byeong.local>
2024-04-29 15:41:55 +0900

    product crate, read

--- products/models.py
+++ products/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=20)
+    content = models.TextField()
+    image = models.ImageField(blank=True)
+    
+    # fireignkey = models.ForeignKey(settings.AUTH_USER_MODEL ,on_delete=models.CASCADE)
--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from . import views
 
 app_name ='produects'
 urlpatterns = [
-
+    path('',views.ProductAPIView.as_view()),
+    # path('<int:productId>/' views.)
 ]
