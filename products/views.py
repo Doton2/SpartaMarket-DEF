365f9519b9b96bd0bca35e3ec8648995eab52c17
김병민 <gimbyeongmin@byeong.local>
2024-05-01 03:36:52 +0900

    상품 목록조 paginator 구현

--- products/views.py
+++ products/views.py
@@ -2,13 +2,15 @@ from django.shortcuts import get_object_or_404, render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.decorators import permission_classes
-from rest_framework.permissions import IsAuthenticated,DjangoModelPermissions
+from rest_framework.permissions import IsAuthenticated
 from .serializers import ProductSerializer
-from rest_framework import status
+from rest_framework import status, viewsets
 from .models import Product
+from rest_framework.pagination import PageNumberPagination
 
 # Create your views here.
 class ProductAPIView(APIView):
+
     @permission_classes([IsAuthenticated])
     def post(self, request):
         serializer = ProductSerializer(data=request.data)
@@ -16,11 +18,12 @@ class ProductAPIView(APIView):
             serializer.save(author_id=request.user.id)
             return Response(serializer.data ,status=status.HTTP_200_OK)
         
-
     def get(self, request):
         products = Product.objects.all()
-        serializer = ProductSerializer(products, many=True)
-        return Response(serializer.data)
+        paginator = PageNumberPagination()
+        res_pag = paginator.paginate_queryset(products, request)
+        serializer = ProductSerializer(res_pag, many=True)
+        return paginator.get_paginated_response(serializer.data)
     
 
 class ProductDetail(APIView):
--- spartamarket_drf/settings.py
+++ spartamarket_drf/settings.py
@@ -94,6 +94,8 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 2,
 }
 
 
